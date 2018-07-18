f5667dc153a9ecef6b10976204a19bbf5b098b26
yy-web <403086967@qq.com>
2018-07-18 20:31:51 +0800

    第一次审核

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,5 +1,5 @@
 export default Object.assign({}, {
-  path: 'http://beta-sdkhealth.healthmall.cn/activity-service',
+  path: 'https://beta-sdkhealth.healthmall.cn/activity-service',
   codeVersion: '2.3.0', //代码版本 用于审核
   reportAnalysisBaseUrl:'https://enterapp.sunlands.com/datacenter_app/dataService' // 数据上报的地址
 }, __ENV__ === 'production' ? {
